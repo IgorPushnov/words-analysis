bc8296b8403e924682bed545034366d67e43fc1f
IgorPushnov <47480817+IgorPushnov@users.noreply.github.com>
2021-05-10 18:19:22 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const textExample = "Hello world Hello all";
+
+function calculateWordsStatistic(text) {
+  function parseWords(str) {
+    return str.split(" ");
+  }  
+  
+  function collectStatistic(array) {
+    var statistic = {};
+    array.forEach(function(x) { statistic[x] = (statistic[x] || 0)+1; });
+    return statistic;
+  }
+    return collectStatistic(parseWords(text));
+}
+
+console.log(calculateWordsStatistic(textExample));
