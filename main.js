2704a7356acd1143a86ef3e11654ac52a84078eb
IgorPushnov <Kompanella48@gmail.com>
2021-05-13 14:55:44 +0300

    adding starting project

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ const textExample = "Hello world Hello all";
 function calculateWordsStatistic(text) {
   function parseWords(str) {
     return str.split(" ");
-  }  
-  
+  }
+
   function collectStatistic(array) {
     var statistic = {};
-    array.forEach(function(x) { statistic[x] = (statistic[x] || 0)+1; });
+    array.forEach(function (x) { statistic[x] = (statistic[x] || 0) + 1; });
     return statistic;
   }
-    return collectStatistic(parseWords(text));
+
+  return collectStatistic(parseWords(text));
 }
 
 console.log(calculateWordsStatistic(textExample));
