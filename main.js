ebc85f7c87ffbb3acc5b0c6ecc9dce843aeb5094
IgorPushnov <Kompanella48@gmail.com>
2021-05-13 16:56:54 +0300

    added new files

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const textExample = "Hello world Hello all";
+const fs = require('fs');
+
+const array = fs.readFileSync('dog.css').toString().split(" ");
+// for (i in array) {
+//   console.log(array[i]);
+// }
+
+const textExample = array.join(" ");
 
 function calculateWordsStatistic(text) {
   function parseWords(str) {
@@ -10,8 +17,8 @@ function calculateWordsStatistic(text) {
     array.forEach(function (x) { statistic[x] = (statistic[x] || 0) + 1; });
     return statistic;
   }
-
   return collectStatistic(parseWords(text));
 }
 
-console.log(calculateWordsStatistic(textExample));
+console.log(textExample);
+// console.log(calculateWordsStatistic(textExample));
