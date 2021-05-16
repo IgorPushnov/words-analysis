dbd92925ea099583a6ce7edefdcd8b8447862b2f
IgorPushnov <Kompanella48@gmail.com>
2021-05-16 18:43:14 +0300

    Changes are made to comments

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const textExample1 = fs.readFileSync('testData/cat.html', 'utf-8');
 const textExample2 = fs.readFileSync('testData/dog.css', 'utf-8');
 const textExample3 = fs.readFileSync('testData/world.txt', 'utf-8');
-const addText = new Array(textExample1, textExample2, textExample3);
+const textsForAnalysis = new Array(textExample1, textExample2, textExample3);
 
-function calculateWordsStatistic(text) {
+function calculateWordsStatistic(objectForAnalysis) {
   function parseWords(str) {
-    return str.toString().replace(/\<+.+?(?=\>)|\>|\/|\W|\s/g, " ").replace(/\s+/g, " ").split(" ");
+    return str.replace(/\<+.+?(?=\>)|\>|\/|\W|\s/g, " ").replace(/\s+/g, " ").split(" ");
   }
 
   function collectStatistic(array) {
@@ -15,11 +15,11 @@ function calculateWordsStatistic(text) {
     array.forEach(function (x) { statistic[x] = (statistic[x] || 0) + 1; });
     return statistic;
   }
-  return collectStatistic(parseWords(text));
+  return collectStatistic(parseWords(objectForAnalysis.toString()));
 }
 
 // console.log(textExample1);
 // console.log(textExample2);
 // console.log(textExample3);
-// console.log(addText);
-console.log(calculateWordsStatistic(addText));
+// console.log(textsForAnalysis);
+console.log(calculateWordsStatistic(textsForAnalysis));
