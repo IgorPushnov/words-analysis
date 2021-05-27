5b9d53e4fd99faa0cd8712adb00b7b0283fec92b
IgorPushnov <Kompanella48@gmail.com>
2021-05-27 18:31:30 +0300

    сhanged code

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const readWorld = require('./readfiles.js');
 const calculateWordsStatistic = require('./analysis.js');
 
-// console.log(calculateWordsStatistic(readWorld('testData')));
+console.log(calculateWordsStatistic(readWorld('testData')));
 
-module.exports = readWorld;
-module.exports = calculateWordsStatistic;
--- main.test.js
+++ main.test.js
@@ -1,38 +1,4 @@
-const readWorld = require('./main');
-const calculateWordsStatistic = require('./main');
-
-test('string in the number of words', () => {
-	expect(calculateWordsStatistic(readWorld('testData')).toEqual
-		({
-			'Document' : 1,
-			'New' : 2,
-			'story' : 2,
-			'h1' : 1,
-			'color' : 1,
-			'green' : 2,
-			'block' : 1,
-			'div' : 1,
-			'width' : 1,
-			'148px' : 1,
-			'var' : 1,
-			'fs' : 3,
-			'require' : 1,
-			'function' : 3,
-			'createFile' : 2,
-			'filePath' : 2,
-			'fileContent' : 2,
-			'appendFile' : 1,
-			'err' : 3,
-			'if' : 1,
-			'throw' : 1,
-			'console' : 1,
-			'log' : 1,
-			'Saved' : 1,
-			'module' : 1,
-			'exports' : 1,
-			'Hello' : 2,
-			'world' : 1,
-			'all' : 1
-		})
-	)
+test('check simple text statistic', () => {
+	const testText = "Some test text, lala, test";
+	expect(calculateWordsStatistic(testText)).toEqual({ 'some': 1, 'test': 2, 'text': 1, 'lala': 1 });
 })
