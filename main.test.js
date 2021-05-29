ec695a62f3458eadfa100019fa71df4d5f1a1e81
IgorPushnov <Kompanella48@gmail.com>
2021-05-29 16:02:45 +0300

    added changes

--- main.test.js
+++ main.test.js
@@ -1,4 +1,6 @@
+const calculateWordsStatistic = require('./analysis.js');
+
 test('check simple text statistic', () => {
-	const testText = "Some test text, lala, test";
+	const testText = "some test text, lala, test";
 	expect(calculateWordsStatistic(testText)).toEqual({ 'some': 1, 'test': 2, 'text': 1, 'lala': 1 });
 })
