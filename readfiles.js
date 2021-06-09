ef18716a88fd9036772fb27f50f4c3980ca74795
IgorPushnov <Kompanella48@gmail.com>
2021-06-09 16:14:35 +0300

    сhanged function readfiles.js and аdded test files

--- readfiles.js
+++ readfiles.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
-const path = require('path');
 
-function readFiles(folderPath) {
-  const pathFiles = fs.readdirSync(folderPath).map(fileName => path.join(folderPath, fileName));
+function readFiles(folderPath, pathFiles) {
+  pathFiles = pathFiles || [];
+  var files = fs.readdirSync(folderPath);
+  for (var i in files) {
+    var name = folderPath + '/' + files[i];
+    if (fs.statSync(name).isDirectory()) {
+      readFiles(name, pathFiles);
+    } else {
+      pathFiles.push(name);
+    }
+  }
   return pathFiles.map(pathToFile => fs.readFileSync(pathToFile, 'utf-8'));
 }
+
 module.exports = readFiles;
